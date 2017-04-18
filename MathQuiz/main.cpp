c233633c84fbfe74b16ee2aa1707d94bff0fdfd2
Gamaliel Padillo <gamalielgad1234@gmail.com>
2017-04-17 23:20:27 -0700

    First commit

---
+++ MathQuiz/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MathQuiz
+//
+//  Created by Gamaliel Padillo on 4/17/17.
+//  Copyright © 2017 Gamaliel Padillo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Math Quiz Midterm for CompSci 139 C++
+This file repo holds my midterm for C++
+
+
+## How to use
+Open your IDE and copy the code, run it.
+
+## Motivation
+Hopefully an A
+
+## Contributors
+ALL ME.
+
+## License
+Licensed under MIT license. 
