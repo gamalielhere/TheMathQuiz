673bb90fdd539094579751ba1c5c8e363e455c02
Gamaliel Padillo <gamalielgad1234@gmail.com>
2017-04-20 01:05:24 -0700

    Add more functionality to menu

--- MathQuiz/main.cpp
+++ MathQuiz/main.cpp
@@ -12,7 +12,7 @@
 #include <cctype>
 using namespace std;
 
-void validateUserResponse(bool);
+// void validateUserResponse(int);
 void credits(char &, string &);
 void clearScreen();
 void retrieveStats(string &);
@@ -30,12 +30,14 @@ void credits(char &userResponse, string &userName) {
   // Creating the menu box
   cout << string(24, '*') << endl;
   cout << string(24, '*') << endl;
+  cout << string(24, '*') << endl;
   cout << string(6, '*') << string(12, ' ') << string(6, '*') << endl;
   cout << string(6, '*') << string(4, ' ') << " The" << string(4, ' ') << string(6, '*') << endl;
   cout << string(6, '*') << "  MathGame  " << string(6, '*') << endl;
   cout << string(6, '*') << string(4, ' ') << " By " << string(4, ' ') << string(6, '*') << endl;
   cout << string(6, '*') << "  Gamaliel  " << string(6, '*') << endl;
   cout << string(6, '*') << string(12, ' ') << string(6, '*') << endl;
+  cout << string(24, '*') << endl;
   cout << string(24, '*') << endl;
   cout << string(24, '*') << endl;
   cout << endl;
@@ -57,6 +59,7 @@ void retrieveStats(string &userName) {
   bool validName = false;
   int stringErrorCounter = 0;
   string userFileName;
+  ofstream newUserFile;
 
   cin.ignore();
   cout << "Enter your name and press <ENTER> \n";
@@ -77,21 +80,72 @@ void retrieveStats(string &userName) {
       validName = true;
     }
   }
-}
 
-void menu (string userName) {
-  string textLine, userFileName;
   userFileName = userName + ".txt";
   ifstream userFile(userFileName);
-  ofstream newUserFile;
 
   if (!userFile) {
     newUserFile.open(userFileName);
-    newUserFile << userName;
+    newUserFile << userName << endl << 0 << endl << 0 << endl << 0 << endl;
     newUserFile.close();
-  } else {
-    while (getline(userFile, textLine)) { // loop through file by string
-      cout << textLine << endl;
-    }
   }
+}
+
+void menu (string userName) {
+  char userInput;
+  bool quizEnd = false;
+
+  do {
+    cout << string(4, '*') << " CHOOSE A PROBLEM " << string(3, '*') << endl;
+    cout << string(25, '*') << endl;
+    cout << string(25, '*') << endl;
+    cout << string(6, '*') << string(13, ' ') << string(6, '*') << endl;
+    cout << string(6, '*') << " 1. ADD      " << string(6, '*') << endl;
+    cout << string(6, '*') << " 2. SUBTRACT " << string(6, '*') << endl;
+    cout << string(6, '*') << " 3. MULTIPLY " << string(6, '*') << endl;
+    cout << string(6, '*') << " 4. DIVIDE   " << string(6, '*') << endl;
+    cout << string(6, '*') << " 5. STATS    " << string(6, '*') << endl;
+    cout << string(6, '*') << " n/N to QUIT " << string(6, '*') << endl;
+    cout << string(6, '*') << string(13, ' ') << string(6, '*') << endl;
+    cout << string(25, '*') << endl;
+    cout << string(25, '*') << endl;
+    cin >> userInput;
+
+    switch (toupper(userInput)) {
+      case '1':
+        cout << userInput << endl; // Generate the problems here instead.
+        cin.clear();
+        clearScreen();
+        break;
+      case '2':
+        cout << userInput << endl; // Generate the problems here instead.
+        cin.clear();
+        clearScreen();
+        break;
+      case '3':
+        cout << userInput << endl; // Generate the problems here instead.
+        cin.clear();
+        clearScreen();
+        break;
+      case '4':
+        cout << userInput << endl; // Generate the problems here instead.
+        cin.clear();
+        clearScreen();
+        break;
+      case '5':
+        cout << userInput << endl; // Generate the problems here instead.
+        cin.clear();
+        clearScreen();
+        break;
+      case 'N':
+        quizEnd = true;
+        clearScreen();
+        break;
+      default:
+        cout << "This is not a valid menu option \n";
+        cin.clear();
+        break;
+    }
+  } while(!quizEnd);
+
 }
